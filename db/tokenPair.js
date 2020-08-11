6669a4f579c56777057c418c40dfc4cd60217970
codeoneline <mutouji1@163.com>
2020-08-11 19:03:58 +0800

    add for 0.4 old test

--- db/tokenPair.js
+++ db/tokenPair.js
@@ -12,93 +12,33 @@ const chainId = {
 }
 
 module.exports = {
-  ETH2WAN: {
-    mapChain: 'WAN',
-    originChain: 'ETH',
-    mapToken: {name: 'wan map eth', symbol: 'wanETH', decimals: 18},
-    originToken: {name: 'eth', symbol: 'ETH', decimals: 18},
-    pair: {
-      id: 1,
-      aInfo: [aAccount, "eth", "ETH", 18, 0x8000003c],
-      fromChainID: 0x8000003c,
-      fromAccount: fromAccount,
-      toChainID: 0x8057414e,
-      tokenAddress: '0x36FfEcE47A3BaF210b26cc469E37eef2212d9812',
-    },
-  },
-
-  WAN2ETH: {
+  BTC2ETH: {
     mapChain: 'ETH',
     originChain: 'WAN',
-    mapToken: {name: 'eth map wan', symbol: 'WAN', decimals: 18},
-    originToken: {name: 'wan', symbol: 'WAN', decimals: 18},
+    mapToken: {name: 'btc map eth', symbol: 'wanBTC', decimals: 18},
+    originToken: {name: 'btc on wan', symbol: 'BTC', decimals: 18},
     pair: {
-      id: 2,
-      aInfo: [aAccount, "wan", "WAN", 18, 0x8057414e],
+      id: 22,
+      aInfo: [aAccount, "btc", "BTC", 18, 0x8057414e],
       fromChainID: 0x8057414e,
       fromAccount: fromAccount,
       toChainID: 0x8000003c,
-      tokenAddress: '0xe87627630583e5411486982268b01DaBcaB5CeC0',
+      tokenAddress: '0x0000000000000000000000000000000000000000',
     },
   },
 
-  ETC2WAN: {
-    mapChain: 'WAN',
-    originChain: 'ETC',
-    mapToken: {name: 'etc map to wan', symbol: 'wanETC', decimals: 18},
-    originToken: {name: 'etc', symbol: 'ETC', decimals: 18},
-    pair: {
-      id: 5,
-      aInfo: [aAccount, "etc", "ETC", 18, 0x8000003d],
-      fromChainID: 0x8000003d,
-      fromAccount: fromAccount,
-      toChainID: 0x8057414e,
-      tokenAddress: '0x886befdc63a010badbaecc6b089cb7dd835200f4',
-    },
-  },
-
-  WAN2ETC: {
-    mapChain: 'ETC',
-    originChain: 'WAN',
-    mapToken: {name: 'wan map to etc', symbol: 'WAN', decimals: 18},
-    originToken: {name: 'wan', symbol: 'WAN', decimals: 18},
-    pair: {
-      id: 6,
-      aInfo: [aAccount, "wan", "WAN", 18, 0x8057414e],
-      fromChainID: 0x8057414e,
-      fromAccount: fromAccount,
-      toChainID: 0x8000003d,
-      tokenAddress: '0xF2f5C0c525231561ad19103E70a052F7EA519569',
-    },
-  },
-
-  LINK2WAN: {
-    mapChain: 'WAN',
-    originChain: 'ETH',
-    mapToken: {name: 'link map on wan', symbol: 'wanLINK', decimals: 18},
-    originToken: {name: 'link on eth', symbol: 'LINK', decimals: 18},
-    pair: {
-      id: 3,
-      aInfo: [hexToBytes("0x64993826cDf00B4355C4f366e2C38da140Eb5f0D"), "link on eth", "LINK", 18, 0x8000003c],
-      fromChainID: 0x8000003c,
-      fromAccount: hexToBytes("0x64993826cDf00B4355C4f366e2C38da140Eb5f0D"),
-      toChainID: 0x8057414e,
-      tokenAddress: '0x1A5a44D553Df414920874042b8324Cb63F11e917',
-    },
-  },
-
-  FNX2ETH: {
+  EOS2ETH: {
     mapChain: 'ETH',
     originChain: 'WAN',
-    mapToken: {name: 'fnx map on eth', symbol: 'wanFNX', decimals: 18},
-    originToken: {name: 'fnx on wan ', symbol: 'FNX', decimals: 18},
+    mapToken: {name: 'eos map eth', symbol: 'wanEOS', decimals: 18},
+    originToken: {name: 'eos on wan', symbol: 'EOS', decimals: 18},
     pair: {
-      id: 4,
-      aInfo: [hexToBytes("0x3F759314c81F078b9BaAb7a1fddD6758f027D333"), "fnx on wan", "FNX", 18, 0x8057414e],
+      id: 23,
+      aInfo: [aAccount, "eos", "EOS", 18, 0x8057414e],
       fromChainID: 0x8057414e,
-      fromAccount: hexToBytes("0x3F759314c81F078b9BaAb7a1fddD6758f027D333"),
+      fromAccount: fromAccount,
       toChainID: 0x8000003c,
-      tokenAddress: '0x132329E7e4CD25f4CcAE33d40B4eb40006f1Fb52',
+      tokenAddress: '0x0000000000000000000000000000000000000000',
     },
   }
 }
