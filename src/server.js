64cf58edcf527efd2b082fb078d77c591f4e7154
codeoneline <mutouji1@163.com>
2020-09-02 14:31:32 +0800

    set server port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3200
+const port = 13200
 
 app.all('*', (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
