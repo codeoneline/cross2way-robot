fe890c2bdfd5d2dbc1e2a47ae89bd42a77547a3c
codeoneline <mutouji1@163.com>
2020-09-10 17:49:12 +0800

    update token address

--- src/admin.js
+++ src/admin.js
@@ -77,7 +77,7 @@ setTimeout( async () => {
   // await wanEth.update('WAN@ethereum', 'WAN', {from: "0x9da26fc2e1d6ad9fdd46138906b0104ae68a65d8"});
 
   // await mint([fnxWan, btcWan, eosWan], "0xFB683bDDB0ACBB00Dd162CD5E3798c7Fc6E5CFc0".toLowerCase(), 1000);
-  await mint([fnxWan], "0x0d5A1204c001693D86E70566Ae57D49F40d04C90".toLowerCase(), 10000000);
+  // await mint([fnxWan], "0x0d5A1204c001693D86E70566Ae57D49F40d04C90".toLowerCase(), 10000000);
   // await mint([linkEth], "0xded23dd19136574fce6b4ab4ea76395c4088a033", 100000000);
   // await mint([linkEth], "0xded23dd19136574fce6b4ab4ea76395c4088a033", 100000000);
   // await mint([wanEth], "0x67e3b428acbc3aa2fd38813f65dafbd5af97c6d5", 100000000);
