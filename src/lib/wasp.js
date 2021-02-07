2bc42f91c8bc92fbc98c12e59a70e705a0ea5dab
codeoneline <mutouji1@163.com>
2021-02-07 16:17:17 +0800

    move wasp price to a single file

---
+++ src/lib/wasp.js
@@ -0,0 +1,71 @@
+// get price from graphql
+const { fractionRatioToDecimalString } = require('./utils');
+const Web3 = require('web3');
+const BigNumber = require('bignumber.js');
+
+// const fetch = require('node-fetch')
+// const { ApolloClient } = require('apollo-client')
+// const { InMemoryCache } = require('apollo-cache-inmemory')
+// const { createHttpLink } = require('apollo-link-http')
+// const gql = require('graphql-tag')
+// // price
+// // {
+// //   "finnexus": {
+// //     "usd": 0.120816
+// //   },
+// //   "wanchain": {
+// //     "usd": 0.370103
+// //   }
+// // }
+
+// function TOKEN_DATA() {
+//   return gql`query tokens {
+//     tokens(where:{symbol: "WASP"}) {
+//       id
+//       symbol
+//       derivedETH
+//     }
+//   }`
+// }
+
+// const client = new ApolloClient({
+//   link: new createHttpLink({
+//     uri: 'https://graph.wanswap.finance/subgraphs/name/wanswap/wanswap-subgraph-3',
+//     fetch: fetch
+//   }),
+//   cache: new InMemoryCache(),
+//   shouldBatch: true,
+// })
+
+// async function getWaspPriceFromGraphql() {
+//   const query = TOKEN_DATA()
+//   const p = await client.query({
+//     query: query,
+//     fetchPolicy: 'cache-first',
+//   })
+//   console.log(JSON.stringify(p, null, 2))
+//   const waspPrice = fractionRatioToDecimalString(p.data.tokens[0].derivedETH, 18, priceMap['WAN'])
+//   return waspPrice
+// }
+
+const abi = [{"constant":true,"inputs":[],"name":"getReserves","outputs":[{"internalType":"uint112","name":"_reserve0","type":"uint112"},{"internalType":"uint112","name":"_reserve1","type":"uint112"},{"internalType":"uint32","name":"_blockTimestampLast","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"}];
+const address = "0x29239a9b93a78decec6e0dd58ddbb854b7ffb0af";
+async function getWaspPriceFromContract(wanPrice) {
+    const web3 = new Web3(new Web3.providers.HttpProvider('https://gwan-ssl.wandevs.org:56891'));
+    const sc = new web3.eth.Contract(abi, address);
+    let ret = await sc.methods.getReserves().call();
+    console.log('ret', ret);
+    const wp = new BigNumber(wanPrice)
+    const wsp = new BigNumber(ret._reserve1)
+    const wan = new BigNumber(ret._reserve0)
+    let p = wsp.div(wan).multipliedBy(wp).integerValue();
+
+    console.log("p", p.toString());
+
+    return '0x' + p.toString(16)
+}
+
+module.exports = {
+  // getWaspPriceFromGraphql,
+  getWaspPriceFromContract
+}
